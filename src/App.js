4778f5d334900d42f057d767c7c2a42127bac9e1
atahhabibi <habibiatah@gmail.com>
2022-11-15 16:38:47 -0800

    completing Redux toolKit small project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,45 @@
+import Navbar from "./components/Navbar";
+import {useEffect} from 'react'
+import CartContainer from "./components/CartContainer";
+import {useDispatch,useSelector} from 'react-redux';
+import {calculateTotals,getCartItems} from './features/Cart/cartSlice'
+import Modal from './components/Modal'
+import axios from 'axios'
+
+
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const dispatch=useDispatch();
+
+  const {cartItems,isLoading}=useSelector((store)=>store.cart)
+  const {isOpen}=useSelector((store)=>store.modal)
+
+
+  useEffect(()=>{
+   dispatch(calculateTotals())
+  },[cartItems])
+
+
+  useEffect(()=>{
+    dispatch(getCartItems())
+  },[])
+
+
+  if(isLoading){
+    return <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  }
+
+
+
+
+ 
+  return <main>
+     {isOpen && <Modal/>}
+     <Navbar/>
+     <CartContainer/>
+     
+</main>;
 }
 export default App;
